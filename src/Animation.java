d6981dedc28e87c60c40926e0957fb9075ad4101
Hien-Minh <htrinh@macalester.edu>
2024-04-29 10:01:30 -0500

    Submit version

--- src/Animation.java
+++ src/Animation.java
@@ -5,6 +5,14 @@ public class Animation {
     private double x, y, end_x, end_y, step_x, step_y;
     private int duration;
 
+    /**
+     * Creates an animation for a disk to move from its current position to a new position
+     * 
+     * @param disk     the disk to be moved
+     * @param end_x    the x-coordinate of the new position
+     * @param end_y    the y-coordinate of the new position
+     * @param duration the number of frames the animation should last
+     */
     public Animation(Rectangle disk, double end_x, double end_y, int duration) {
         this.disk = disk;
         this.x = disk.getCenter().getX();
@@ -16,12 +24,18 @@ public class Animation {
         this.step_y = (end_y - y) / duration;
     }
 
+    /**
+     * Move the disk by one frame
+     */
     public void run() {
         x += step_x;
         y += step_y;
         disk.setCenter(x, y);
     }
 
+    /**
+     * Update the step size of the disk
+     */
     public void update() {
         this.x = disk.getCenter().getX();
         this.y = disk.getCenter().getY();
@@ -29,6 +43,11 @@ public class Animation {
         this.step_y = (end_y - y) / duration;
     }
 
+    /**
+     * Check if the animation is done
+     * 
+     * @return true if the animation is done
+     */
     public boolean isDone() {
         boolean x_done = Math.abs(disk.getCenter().getX() - end_x) < 0.1;
         boolean y_done = Math.abs(disk.getCenter().getY() - end_y) < 0.1;
